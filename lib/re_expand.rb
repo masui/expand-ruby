31a44bf95351b1d8248207db7f914750a17d175c
Toshiyuki Masui <masui@pitecan.com>
2012-04-30 11:56:58 +0900

    initial version

---
+++ lib/re_expand.rb
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+$:.unshift(File.dirname(__FILE__)) unless
+  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+
+module ReExpand
+  VERSION = '0.0.1'
+end
+
+require 'Generator'
+require 'Node'
+require 'Asearch'
+require 'Scanner'
+
+class String
+  # ExpandHelp用のライブラリを利用
+  def expand(filterpat=' ')
+    g = Generator.new
+    g.add(self,'')
+    strings = []
+    if filterpat.class == String then
+      matched = g.generate(filterpat)
+      res = matched[0].length > 0 ? matched[0] : matched[1].length > 0 ? matched[1] : matched[2]
+      strings = res.collect { |r|
+        r[0]
+      }
+    elsif
+      matched = g.generate(' ')
+      strings = matched[0].collect { |r|
+        r[0]
+      }
+      if filterpat.class == Regexp then
+        strings = strings.find_all { |s|
+          filterpat.match(s)
+        }
+      end
+    end
+
+    if block_given? then
+      strings.each { |string|
+        yield string
+      }
+    else
+      strings
+    end
+  end
+end
+
